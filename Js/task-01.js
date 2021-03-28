f794d7363dda307bd52349d43928d0b867bac40e
Yrii <hvozdetskiyyrii@gmail.com>
2021-03-28 19:13:23 +0300

    task-01

--- Js/task-01.js
+++ Js/task-01.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const listCategoriesRef = document.querySelector('#categories');
+
+const showlistCategoriesItems = (list) => `В списке ${list.children.length} категории.`;
+
+console.log(showlistCategoriesItems(listCategoriesRef));
+
+const itemRef = document.querySelectorAll('.item');
+
+const showСategoryAndQuantity = (item) => {
+    return [...item]
+        .map(
+            (el) =>
+                ` Категория: ${el.firstElementChild.textContent},количество элементов:${el.lastElementChild.children.length}`,
+        )
+        .join(',')
+        .trim();
+};
+
+console.log(showСategoryAndQuantity(itemRef));
