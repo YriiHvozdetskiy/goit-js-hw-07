9eec2086246fafd26fe8944d761b76c4c639c720
Yrii <hvozdetskiyyrii@gmail.com>
2021-04-02 20:59:21 +0300

    task-08

--- Js/task-08.js
+++ Js/task-08.js
@@ -1,14 +1,38 @@
 const refs = {
     controls: document.querySelector('#controls'),
+    input: document.querySelector('#controls input'),
     renderBtn: document.querySelector('[data-action="render"]'),
-    destroyBtn: document.querySelector('[data-action="render"]'),
-    boxes: document.querySelector('.boxes'),
+    destroyBtn: document.querySelector('[data-action="destroy"]'),
+    boxes: document.querySelector('#boxes'),
 };
 
 refs.renderBtn.addEventListener('click', createBoxes);
-
-const inputValueRef = refs.controls.firstElementChild.value;
+refs.destroyBtn.addEventListener('click', onDestroy);
 
 function createBoxes(amount) {
-    console.log(inputValueRef);
+    if (refs.boxes.children.length >= 1) {
+        let elem = document.createElement('div');
+        elem.classList.add('box');
+        let counter = 10;
+        let widthNumber = Number.parseInt(refs.boxes.lastElementChild.style.width);
+        let heightNumber = Number.parseInt(refs.boxes.lastElementChild.style.width);
+
+        elem.style.width = widthNumber + counter + 'px';
+        elem.style.height = heightNumber + counter + 'px';
+
+        return refs.boxes.append(elem);
+    }
+
+    let elem = document.createElement('div');
+    elem.classList.add('box');
+    elem.style.width = '30px';
+    elem.style.height = '30px';
+
+    return refs.boxes.append(elem);
+}
+
+function onDestroy() {
+    refs.boxes.remove();
+
+    refs.controls.insertAdjacentHTML('afterend', '<div id="boxes"></div>');
 }
