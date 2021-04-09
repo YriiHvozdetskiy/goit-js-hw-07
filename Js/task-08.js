7cf2c6f925ea776155a3dd06079593346111a0c5
Yrii <hvozdetskiyyrii@gmail.com>
2021-04-09 14:27:25 +0300

    task-08 improм

--- Js/task-08.js
+++ Js/task-08.js
@@ -4,20 +4,33 @@ const refs = {
     renderBtn: document.querySelector('[data-action="render"]'),
     destroyBtn: document.querySelector('[data-action="destroy"]'),
     boxes: document.querySelector('#boxes'),
+    scatter: document.querySelector('[data-action="scatter"]'),
 };
 
 refs.renderBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', onDestroy);
+refs.scatter.addEventListener('click', onRendomPosition);
+
+const screenWidth = window.screen.width / 2 - 10;
+const screenHeight = window.screen.height / 2 - 10;
 
 function createBoxes(amount) {
+    if (refs.boxes.querySelectorAll('div').length > 0) onDestroy();
+
     amount = refs.input.value;
+
     for (let i = 0; i < amount; i += 1) {
         const basicSize = 30;
         const biggestSize = basicSize + i * 10;
         const box = document.createElement('div');
+        box.classList.add('box');
         box.setAttribute(
             'style',
-            `width: ${biggestSize}px; height: ${biggestSize}px; background-color: ${randomRgb()}`,
+            `width: ${biggestSize}px; height: ${biggestSize}px; background-color: ${randomRgb()}; left:${randomInteger(
+                10,
+                500,
+            )}px;
+            top:${randomInteger(10, 500)}px`,
         );
         boxes.append(box);
     }
@@ -33,4 +46,17 @@ const randomRgb = () => {
 
 function onDestroy() {
     refs.boxes.innerHTML = '';
+}
+
+function onRendomPosition() {
+    const allBoxRef = refs.boxes.querySelectorAll('div');
+
+    for (const box of allBoxRef) {
+        box.classList.add('random');
+    }
+}
+
+function randomInteger(screenWidth, screenHeight) {
+    let rand = screenWidth + Math.random() * (screenHeight + 1 - screenWidth);
+    return Math.floor(rand);
 }
