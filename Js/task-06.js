f96a0d2957747fbafc88bbf0d01236691169421d
Yrii <hvozdetskiyyrii@gmail.com>
2021-04-12 08:18:02 +0300

    improv task-06

--- Js/task-06.js
+++ Js/task-06.js
@@ -4,18 +4,6 @@ const refs = {
 refs.inputText.addEventListener('blur', onFocusChange);
 
 const dataLength = Number(refs.inputText.dataset.length);
-console.log(dataLength);
-
-function onFocusChange(event) {
-    //! Якщо замінити "<" на строге равенство буде не коректно код працювати
-    if (event.currentTarget.value.trim().length < dataLength) {
-        invalidClass();
-    }
-    if (event.currentTarget.value.trim().length >= dataLength) {
-        validClass();
-    }
-    // !Якщо зробити if else код не буде коректно працювати
-}
 
 function invalidClass() {
     refs.inputText.classList.remove('valid');
@@ -25,3 +13,29 @@ function validClass() {
     refs.inputText.classList.remove('invalid');
     refs.inputText.classList.add('valid');
 }
+
+function onFocusChange(event) {
+    if (event.target.value.length === dataLength) {
+        updateClass('valid', 'invalid');
+    } else {
+        updateClass('invalid', 'valid');
+    }
+}
+
+function updateClass(addClass, remClass) {
+    refs.inputText.classList.remove(remClass);
+    refs.inputText.classList.add(addClass);
+}
+
+// ==== моє рішення
+
+// function onFocusChange(event) {
+//     //! Якщо замінити "<" на строге равенство буде не коректно код працювати
+//     if (event.currentTarget.value.trim().length < dataLength) {
+//         invalidClass();
+//     }
+//     if (event.currentTarget.value.trim().length >= dataLength) {
+//         validClass();
+//     }
+// !Якщо зробити if else код не буде коректно працювати
+// }
